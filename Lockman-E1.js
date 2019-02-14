eba2faf1b62bbd218389ddfc1c844d967c5cfb70
Andrew Lockman <a30106194@mcla.edu>
2019-02-14 16:14:30 +0000

    done

---
+++ Lockman-E1.js
@@ -0,0 +1,88 @@
+var http = require('http');
+var server = http.createServer(requestHandler); 
+server.listen(process.env.PORT, process.env.IP, startHandler);
+
+function startHandler()
+{
+  var addr = server.address();
+  console.log("Server listening at", addr.address + ":" + addr.port);
+}
+
+function requestHandler(req, res) 
+{
+  try
+  {
+    var url = require('url');
+    var url_parts = url.parse(req.url, true);
+    var query = url_parts.query;
+    
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    
+    if (query['cmd'] == undefined)
+      throw Error("A command must be specified");
+      
+    var result = {};
+    if (query['cmd'] == 'calcDistance')
+    {
+      result = calcDistance(query);
+    }
+    else if (query['cmd'] == 'calcCost')
+    {
+      result = calcCost(query);
+    }   
+    else
+    {
+      throw Error("Invalid command: " + query['cmd']);
+    }
+ 
+    res.write(JSON.stringify(result));
+    res.end('');
+  }
+  catch (e)
+  {
+    var error = {'error' : e.message};
+    res.write(JSON.stringify(error));
+    res.end('');
+  }
+}
+
+function calcDistance(query)
+{
+  if (query['budget'] == undefined || query['budget'] <= 0 || isNaN(query['budget']))  
+    throw Error("Invalid value for budget");
+  else if (query['mpg'] == undefined || query['mpg'] <= 0 || isNaN(query['mpg']))  
+    throw Error("Invalid value for mpg");
+  else if (query['fuelCost'] == undefined || query['fuelCost'] <= 0 || isNaN(query['fuelCost']))  
+    throw Error("Invalid value for fuelCost");  
+    
+  var budget = parseInt(query['budget']);    
+  var mpg = parseInt(query['mpg']);
+  var fuelCost = parseInt(query['fuelCost']);     
+  var distance = 0;
+  
+  distance = mpg * (budget/fuelCost);
+    
+  var result = {'Distance' : distance}; 
+  return result;
+}
+
+function calcCost(query)
+{
+  if (query['distance'] == undefined || query['distance'] <= 0 || isNaN(query['distance']))  
+    throw Error("Invalid value for distance");
+  else if (query['mpg'] == undefined || query['mpg'] <= 0 || isNaN(query['mpg']))  
+    throw Error("Invalid value for mpg");
+  else if (query['fuelCost'] == undefined || query['fuelCost'] <= 0 || isNaN(query['fuelCost']))  
+    throw Error("Invalid value for fuelCost");  
+    
+  var distance = parseInt(query['distance']);    
+  var mpg = parseInt(query['mpg']);
+  var fuelCost = parseInt(query['fuelCost']);     
+  var cost = 0;
+  
+  cost = (distance/mpg) * fuelCost; 
+  
+    
+  var result = {'cost' : cost}; 
+  return result;
+}
